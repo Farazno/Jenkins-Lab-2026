86a13e27166c95e5d4a1b6fe68642a59d22bb1d5
Farazno <farazno@gmail.com>
2026-09-10 20:45:33 +0530

    Add health endpoint test

--- app/test_app.py
+++ app/test_app.py
@@ -7,3 +7,12 @@ def test_home_page():
     response = client.get("/")
 
     assert response.status_code == 200
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json == {"status": "healthy"}
